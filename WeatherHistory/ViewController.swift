34ca787ee6e4b7bbfd4021aa88a205b9cd4a02b5
vChorniak <v.chorniak41@gmail.com>
2018-12-18 17:46:18 +0200

    Initial Commit

---
+++ WeatherHistory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherHistory
+//
+//  Created by user on 12/18/18.
+//  Copyright © 2018 Volodymyr Chorniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
